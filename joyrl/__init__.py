9e7b42b7e18b0d81ad586fb6b8ba62b6b72d3b04
johnjim0816 <39483938+johnjim0816@users.noreply.github.com>
2023-05-30 23:50:56 +0800

    add DoubleDQN

--- joyrl/__init__.py
+++ joyrl/__init__.py
@@ -5,13 +5,13 @@ Author: JiangJi
 Email: johnjim0816@gmail.com
 Date: 2023-01-01 16:20:49
 LastEditor: JiangJi
-LastEditTime: 2023-05-29 00:27:00
+LastEditTime: 2023-05-30 23:46:37
 Discription: 
 '''
 from joyrl import algos, config, framework, envs, utils
 from joyrl.run import run
 
-__version__ = "0.2.11"
+__version__ = "0.2.12"
 
 __all__ = [
     "algos",
--- joyrl/algos/DoubleDQN/config.py
+++ joyrl/algos/DoubleDQN/config.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JiangJi
+Email: johnjim0816@gmail.com
+Date: 2023-03-15 22:04:42
+LastEditor: JiangJi
+LastEditTime: 2023-05-30 23:47:43
+Discription: 
+'''
 from config.config import DefaultConfig
-
 class AlgoConfig(DefaultConfig):
+    ''' algorithm parameters
+    '''
     def __init__(self) -> None:
         # set epsilon_start=epsilon_end can obtain fixed epsilon=epsilon_end
         self.epsilon_start = 0.95  # epsilon start value
@@ -11,10 +22,9 @@ class AlgoConfig(DefaultConfig):
         self.buffer_size = 100000  # size of replay buffer
         self.batch_size = 64  # batch size
         self.target_update = 4  # target network update frequency
+        # value network layers config
         self.value_layers = [
-            {'layer_type': 'linear', 'layer_dim': ['n_states', 256],
-             'activation': 'relu'},
-            {'layer_type': 'linear', 'layer_dim': [256, 256],
-             'activation': 'relu'},
-            {'layer_type': 'linear', 'layer_dim': [256, 'n_actions'],
-             'activation': 'none'}]
+            {'layer_type': 'Linear', 'layer_size': [256], 'activation': 'ReLU'},
+            {'layer_type': 'Linear', 'layer_size': [256], 'activation': 'ReLU'},
+        ]
+
