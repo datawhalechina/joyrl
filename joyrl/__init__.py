42d24e4219bcbe2ad0a28f08f9851aa276aaf6ec
johnjim0816 <johnjim0816@gmail.com>
2024-01-26 00:59:34 +0800

    [0.5.0] rlease

--- joyrl/__init__.py
+++ joyrl/__init__.py
@@ -5,13 +5,13 @@ Author: JiangJi
 Email: johnjim0816@gmail.com
 Date: 2023-01-01 16:20:49
 LastEditor: JiangJi
-LastEditTime: 2024-01-13 18:46:34
+LastEditTime: 2024-01-26 00:59:03
 Discription: 
 '''
 from joyrl import algos, framework, envs, utils
 from joyrl.run import run
 
-__version__ = "0.4.9.1"
+__version__ = "0.5.0"
 
 __all__ = [
     "algos",
