[0.5.2] dev: fix logprob with continuous layer

joyrl/algos/PPO/policy.py
@@ -5,7 +5,7 @@ Author: JiangJi
 Email: johnjim0816@gmail.com
 Date: 2023-12-22 23:02:13
 LastEditor: JiangJi
-LastEditTime: 2024-01-26 00:52:28
+LastEditTime: 2024-01-26 13:21:39
 Discription: 
 '''
 import torch
@@ -120,7 +120,7 @@ class Policy(BasePolicy):
         states, actions, next_states, rewards, dones, returns = kwargs.get('states'), kwargs.get('actions'), kwargs.get('next_states'), kwargs.get('rewards'), kwargs.get('dones'), kwargs.get('returns')
         old_log_probs  = kwargs.get('log_probs')
         torch_dataset = Data.TensorDataset(*states, actions, old_log_probs, returns)
-        train_loader = Data.DataLoader(dataset=torch_dataset, batch_size=self.sgd_batch_size, shuffle = False, drop_last=False)
+        train_loader = Data.DataLoader(dataset = torch_dataset, batch_size = self.sgd_batch_size, shuffle = False, drop_last = False)
         for _ in range(self.k_epochs):
             for data in train_loader:
                 old_states = []
@@ -134,21 +134,23 @@ class Policy(BasePolicy):
                 advantages = returns - values.detach() # shape:[batch_size,1]
                 # get action probabilities
                 _ = self.actor(old_states.copy()) # list
-                new_log_probs = self.actor.action_layers.get_log_probs_action(old_actions.squeeze(dim=1)).unsqueeze(dim=1)
+                old_actions = old_actions.squeeze(dim=1)
+                new_log_probs = self.actor.action_layers.get_log_probs_action(old_actions).unsqueeze(dim=1)
                 entropy_mean = self.actor.action_layers.get_mean_entropy()
                 # compute ratio (pi_theta / pi_theta__old):
                 ratio = torch.exp(new_log_probs - old_log_probs) # shape: [batch_size, 1]
                 # compute surrogate loss
                 surr1 = ratio * advantages # shape: [batch_size, 1]
+                # print(surr1.shape, advantages.shape, ratio.shape)
                 surr2 = torch.clamp(ratio, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
                     # compute actor loss
                 self.actor_loss = - (torch.mean(torch.min(surr1, surr2)) + self.entropy_coef * entropy_mean)
                 # compute critic loss
-                self.critic_loss = nn.MSELoss()(returns, values) # shape: [batch_size, 1]
+                self.critic_loss = self.cfg.critic_loss_coef * nn.MSELoss()(returns, values) # shape: [batch_size, 1]
                 # compute total loss
                 if self.share_optimizer:
                     self.optimizer.zero_grad()
-                    self.tot_loss = self.actor_loss + self.critic_loss_coef* self.critic_loss
+                    self.tot_loss = self.actor_loss + self.critic_loss
                     self.tot_loss.backward()
                 else:
                     self.actor_optimizer.zero_grad()

joyrl/algos/base/action_layer.py
@@ -5,7 +5,7 @@ Author: JiangJi
 Email: johnjim0816@gmail.com
 Date: 2023-12-25 09:28:26
 LastEditor: JiangJi
-LastEditTime: 2024-01-26 10:01:05
+LastEditTime: 2024-01-26 13:09:02
 Discription: 
 '''
 from enum import Enum
@@ -38,6 +38,7 @@ class BaseActionLayer(nn.Module):
             return self.predict_action()
         else:
             raise NotImplementedError
+        
 class DQNActionLayer(BaseActionLayer):
     def __init__(self, cfg, input_size, action_dim, id = 0, **kwargs):
         super(DQNActionLayer, self).__init__(cfg=cfg, input_size=input_size, action_dim=action_dim, id=id)
@@ -83,10 +84,6 @@ class DQNActionLayer(BaseActionLayer):
         ''' get action
         '''
         return torch.argmax(self.q_value).detach().cpu().numpy().item()
-
-
-    
-
 
 class DiscreteActionLayer(BaseActionLayer):
     def __init__(self, cfg, input_size, action_dim, id = 0, **kwargs):
@@ -166,13 +163,13 @@ class ContinuousActionLayer(BaseActionLayer):
 
     def forward(self,x, **kwargs):
         mu = self.mu_layer(x)
-        sigma = torch.exp(self.log_std)
+        sigma = torch.ones_like(mu) * torch.exp(self.log_std)
         # log_prob = -0.5 * (sigma.log() + ((mu - x) / sigma).pow(2) + math.log(2 * math.pi))
         # sigma = F.softplus(self.fc4(x)) + 0.001 # std of normal distribution, add a small value to avoid 0
         # sigma = torch.clamp(sigma, min=-0.25, max=0.25) # clamp the std between 0.001 and 1
-        output = {"mu": mu, "sigma": sigma}
-        self.mu = mu
-        self.sigma = sigma
+        self.mu = mu.squeeze(dim=1) # [batch_size]
+        self.sigma = sigma.squeeze(dim=1) # [batch_size]
+        output = {"mu": self.mu, "sigma": self.sigma}
         self.mean = self.mu * self.action_scale + self.action_bias
         self.std = self.sigma
         return output
@@ -198,12 +195,12 @@ class ContinuousActionLayer(BaseActionLayer):
         ''' get log_probs
         '''
         # action shape is [batch_size, action_dim]
-        dist = Normal(self.mean,self.std)
+        dist = Normal(self.mean, self.std)
         if not isinstance(action, torch.Tensor):
             action = torch.tensor(action, dtype=torch.float32, device=self.mean.device)
             # action = action.squeeze(dim=0)
         log_prob = dist.log_prob(action)
-        log_prob = torch.sum(log_prob, dim=1) # sum the log_prob of each action_dim
+
         return log_prob
     
     def get_mean_entropy(self):