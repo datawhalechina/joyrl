2f60d5ca69daaff04365596934f5fcc7c690f49c
johnjim0816 <johnjim0816@gmail.com>
2024-06-17 04:58:13 +0000

    [dev] update sh

--- run.sh
+++ run.sh
@@ -1,9 +1,2 @@
 # conda activate joyrl
-python offline_run.py --yaml presets/ClassControl/Pendulum-v1/Pendulum-v1_DDPG.yaml
-# python -m cProfile -o time.prof offline_run.py --yaml presets/ClassControl/CartPole-v1/CartPole-v1_DQN.yaml 
-# snakeviz output_file.prof
-
-# mprof run offline_run.py --yaml presets/ClassControl/CartPole-v1/CartPole-v1_DQN.yaml
-# mprof plot
-
-# python -m memory_profiler offline_run.py --yaml presets/ClassControl/CartPole-v1/CartPole-v1_DQN.yaml
+python offline_run.py --yaml presets/ClassControl/CartPole-v1/CartPole-v1_DQN.yaml
