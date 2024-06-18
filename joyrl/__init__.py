f81dc60e9aeed2df6001640445940f47aaf38451
johnjim0816 <johnjim0816@gmail.com>
2024-06-18 23:10:30 +0800

    [0.6.5.4] fix bug

--- joyrl/__init__.py
+++ joyrl/__init__.py
@@ -5,13 +5,13 @@ Author: JiangJi
 Email: johnjim0816@gmail.com
 Date: 2023-01-01 16:20:49
 LastEditor: JiangJi
-LastEditTime: 2024-06-18 08:56:38
+LastEditTime: 2024-06-18 23:09:52
 Discription: 
 '''
 from joyrl import algos, framework, envs
 from joyrl.run import run
 
-__version__ = "0.6.5.3"
+__version__ = "0.6.5.4"
 
 __all__ = [
     "algos",
